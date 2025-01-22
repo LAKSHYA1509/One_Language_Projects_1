2e5f7ff816f9a7ff05ac092623f2d3f1bdaec1cc
LAKSHYA BHARDWAJ <lakshyabhardwaj200315@gmail.com>
2025-01-23 00:38:58 +0530

    backend start

---
+++ src/main/java/com/olp_Projects_Rag/Olp_Projects/OlpProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.olp_Projects_Rag.Olp_Projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlpProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlpProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olp_Projects_Rag/Olp_Projects/OlpProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olp_Projects_Rag.Olp_Projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlpProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
